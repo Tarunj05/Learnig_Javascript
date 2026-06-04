ccc1e259e0d3a2ddec577c599a68803a50ab66ca
Tarunj05 <jaiswalt54@gmail.com>
2026-06-05 00:31:10 +0530

    new

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";  //Treat all the code as the newer version of javascript,
+                // not necessarily required but good practice
+
+                
+// alert(3+3)    -> this now gives error in Nodejs(not in browser) but we have a method to use it
+
+console.log(3+0)
+console.log("hitesh")
+
+//use semicolon or use different line,, generally we don't use semicolon
+//Code readability should be high
+
+// tc39.es and mdn will be our goto sites
+
+
+
+
--- 01_basics/test.js
+++
@@ -1,1 +0,0 @@
-console.log("Tarun is god")
